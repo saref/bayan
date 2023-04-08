ec9cf1dd5bddcf9534556e007709ddba3cbfd1d9
mahdi <mahdi.ms86@gmail.com>
2023-04-08 11:14:20 -0400

    restructure

--- example.py
+++
@@ -1,9 +0,0 @@
-from Bayanpy import bayan
-import networkx as nx
-G = nx.karate_club_graph()
-
-
-print(bayan(G))
-
-
-
--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="bayanpy",
-    version="0.1",
+    version="0.4", 
+description="Bayanpy is a powerful Python library for community detection in complex networks, designed to provide optimal or near-optimal solutions for modularity maximization. It features a highly efficient branch-and-cut algorithm and is backed by Integer Programming (IP) formulations.",
     packages=find_packages(),
     install_requires=["requests",
         "pandas",
